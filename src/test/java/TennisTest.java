79b983f7b82b1bfcbfdcaa173bc5142d389c0781
SAR <santiagoalmendrarojas19@gmail.com>
2021-12-16 15:18:43 -0500

    Se acomodó la prueba para TennisGame2

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -21,7 +21,7 @@ public class TennisTest {
         this.player2Score = player2Score;
         this.expectedScore = expectedScore;
     }
-    
+
     @Parameters
     public static Collection<Object[]> getAllScores() {
         return Arrays.asList(new Object[][] {
@@ -30,39 +30,39 @@ public class TennisTest {
                 { 2, 2, "Thirty-All"},
                 { 3, 3, "Deuce"},
                 { 4, 4, "Deuce"},
-                
+
                 { 1, 0, "Fifteen-Love"},
                 { 0, 1, "Love-Fifteen"},
                 { 2, 0, "Thirty-Love"},
                 { 0, 2, "Love-Thirty"},
                 { 3, 0, "Forty-Love"},
                 { 0, 3, "Love-Forty"},
-                { 4, 0, "Win for player1"},
-                { 0, 4, "Win for player2"},
-                
+                { 4, 0, "Win for Santiago"},
+                { 0, 4, "Win for Deison"},
+
                 { 2, 1, "Thirty-Fifteen"},
                 { 1, 2, "Fifteen-Thirty"},
                 { 3, 1, "Forty-Fifteen"},
                 { 1, 3, "Fifteen-Forty"},
-                { 4, 1, "Win for player1"},
-                { 1, 4, "Win for player2"},
+                { 4, 1, "Win for Santiago"},
+                { 1, 4, "Win for Deison"},
 
                 { 3, 2, "Forty-Thirty"},
                 { 2, 3, "Thirty-Forty"},
-                { 4, 2, "Win for player1"},
-                { 2, 4, "Win for player2"},
-                
-                { 4, 3, "Advantage player1"},
-                { 3, 4, "Advantage player2"},
-                { 5, 4, "Advantage player1"},
-                { 4, 5, "Advantage player2"},
-                { 15, 14, "Advantage player1"},
-                { 14, 15, "Advantage player2"},
+                { 4, 2, "Win for Santiago"},
+                { 2, 4, "Win for Deison"},
 
-                { 6, 4, "Win for player1"},
-                { 4, 6, "Win for player2"},
-                { 16, 14, "Win for player1"},
-                { 14, 16, "Win for player2"},
+                { 4, 3, "Advantage Santiago"},
+                { 3, 4, "Advantage Deison"},
+                { 5, 4, "Advantage Santiago"},
+                { 4, 5, "Advantage Deison"},
+                { 15, 14, "Advantage Santiago"},
+                { 14, 15, "Advantage Deison"},
+
+                { 6, 4, "Win for Santiago"},
+                { 4, 6, "Win for Deison"},
+                { 16, 14, "Win for Santiago"},
+                { 14, 16, "Win for Deison"},
         });
     }
 
@@ -70,22 +70,22 @@ public class TennisTest {
         int highestScore = Math.max(this.player1Score, this.player2Score);
         for (int i = 0; i < highestScore; i++) {
             if (i < this.player1Score)
-                game.wonPoint("player1");
+                game.wonPoint("Santiago");
             if (i < this.player2Score)
-                game.wonPoint("player2");
+                game.wonPoint("Deison");
         }
         assertEquals(this.expectedScore, game.getScore());
     }
 
     @Test
     public void checkAllScoresTennisGame1() {
-        TennisGame1 game = new TennisGame1("player1", "player2");
+        TennisGame1 game = new TennisGame1("Santiago", "Deison");
         checkAllScores(game);
     }
 
     @Test
     public void checkAllScoresTennisGame2() {
-        TennisGame2 game = new TennisGame2("player1", "player2");
+        TennisGame2 game = new TennisGame2("Santiago", "Deison");
         checkAllScores(game);
     }
 
